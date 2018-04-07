292126847202f88d8de083290ebcbd031f668bfe
anh.pham <anh.pham@skymapglobal.com>
2018-04-07 17:18:16 +0700

    update.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {AppRoutingModule} from "./app-routing.module"
 import { MainLayoutModule } from './layouts/main-layout/main-layout.module';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
